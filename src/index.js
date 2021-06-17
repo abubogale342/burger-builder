2ee6f2f5565d25deafa82802a2636cce87207d3b
Habtamu Bogale <abubogale342@gmail.com>
2021-06-17 20:24:34 +0300

    basic redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import reducer from './store/reducer'
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 );
